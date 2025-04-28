8510174d3fac38b1b2566fe2649c336ee345b034
Auco77 <allcOliveira77@gmail.com>
2025-04-28 20:39:19 -0300

    Scalar client API OK

--- Ecommerce.OrderService/Program.cs
+++ Ecommerce.OrderService/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+	app.MapScalarApiReference(opt =>
+	{
+		opt.WithTitle("Order API");
+		opt.WithTheme(ScalarTheme.Mars);
+	});
 }
 
 app.UseHttpsRedirection();
