23c162a185f899ebc90b42b9d383f3f6f21697f9
Auco77 <allcOliveira77@gmail.com>
2025-04-28 17:35:27 -0300

    Inferno certs

--- Ecommerce.AppHost/Program.cs
+++ Ecommerce.AppHost/Program.cs
@@ -1,3 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var productApi = builder.AddProject<Projects.Ecommerce_ProductService>("api-product");
+var orderApi = builder.AddProject<Projects.Ecommerce_OrderService>("api-order");
+
+builder.AddProject<Projects.Ecommerce_Web>("front-web")
+	.WithReference(productApi)
+	.WithReference(orderApi);
+
 builder.Build().Run();
